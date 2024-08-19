dc29471a2bf96e7976b9041fa8ad4ac5ee2ccaab
Carlos Soler <carlossoler17@gmail.com>
2024-08-19 11:18:27 -0500

    commit inicial

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,24 @@
+<header class="main-header">
+    <!-- Logotipo -->
+     <a href="" class="logo">
+        <!-- logo mini -->
+         <span class="logo-mini">
+            <img src="vistas/img/plantilla/mini.jpg" alt="Logo" class="img-responsive" style="padding: 10px">
+         </span>
+        <!-- logo normal -->
+         <span class="logo-lg">
+            <img src="vistas/img/plantilla/grande.png" alt="Grande" class="img-responsive" style="padding:10px 0px">
+         </span>
+     </a>
+
+     <!-- Barra de navegacion -->
+     <nav class="navbar navbar-static-top" role="navigation">
+
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+
+      <span class="sr-only">Toggle navigation</span>
+
+      </a>
+
+     </nav>
+</header>
