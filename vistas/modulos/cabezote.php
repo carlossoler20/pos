3179170af8d6dc10841a5e88fe77bd41956de078
Carlos Soler <carlossoler17@gmail.com>
2024-08-19 12:05:19 -0500

    Finalizando video 10

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -1,24 +1,47 @@
 <header class="main-header">
-    <!-- Logotipo -->
-     <a href="" class="logo">
-        <!-- logo mini -->
-         <span class="logo-mini">
-            <img src="vistas/img/plantilla/mini.jpg" alt="Logo" class="img-responsive" style="padding: 10px">
-         </span>
-        <!-- logo normal -->
-         <span class="logo-lg">
-            <img src="vistas/img/plantilla/grande.png" alt="Grande" class="img-responsive" style="padding:10px 0px">
-         </span>
-     </a>
+   <!-- Logotipo -->
+   <a href="" class="logo">
+      <!-- logo mini -->
+      <span class="logo-mini">
+         <img src="vistas/img/plantilla/mini.jpg" alt="Logo" class="img-responsive" style="padding: 10px">
+      </span>
+      <!-- logo normal -->
+      <span class="logo-lg">
+         <img src="vistas/img/plantilla/grande.png" alt="Grande" class="img-responsive" style="padding:10px 0px">
+      </span>
+   </a>
 
-     <!-- Barra de navegacion -->
-     <nav class="navbar navbar-static-top" role="navigation">
+   <!-- Barra de navegacion -->
+   <nav class="navbar navbar-static-top" role="navigation">
+
+      <!-- Boton de navegacion  -->
 
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
 
-      <span class="sr-only">Toggle navigation</span>
+         <span class="sr-only">Toggle navigation</span>
 
       </a>
 
-     </nav>
+      <!-- Perfil de usuario -->
+
+      <div class="navbar-custom-menu">
+         <ul class="nav nav-bar">
+            <li class="drowpdown user user-menu">
+               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  <img src="vistas/img/usuarios/default/anonimo.png" class="user-img" alt="logo-usuario">
+                  <span class="hidden-xs">Usuario Administrador</span>
+               </a>
+               <!-- drowpdown -->
+               <ul class="dropdown-menu">
+                  <li class="user-body">
+                     <div class="pull-right">
+                        <a href="#" class="btn btn-default btn-flat"></a>
+                     </div>
+                  </li>
+               </ul>
+            </li>
+         </ul>
+      </div>
+
+   </nav>
 </header>
